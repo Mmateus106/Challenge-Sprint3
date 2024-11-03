4393747e8045f3361aa9acba33a89f382657db65
Mmateus106 <rm98524@fiap.com.br>
2024-11-03 16:33:30 -0300

    Springboot Actuator implementado ao projeto

--- VomHive/src/main/java/br/com/VomHive/VomHive/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.VomHive.VomHive.repository;
-
-import br.com.VomHive.VomHive.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
